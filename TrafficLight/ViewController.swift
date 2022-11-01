ae56962efab57869f0c0bd271670f72952cb5022
Babasyuk <artem.babasyuk@mail.ru>
2022-11-01 11:20:05 +0300

    Setup properties and methods

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet var redColor: UIView!
+    @IBOutlet var yellowColor: UIView!
+    @IBOutlet var greenColor: UIView!
+    
+    @IBOutlet var switchButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.4
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func startButtonDidTapped() {
+        
+    }
+    
+}
 
+// MARK: - CurrentLight
+extension ViewController {
+    enum CurrentLight {
+        case red, yellow, green
+    }
 }
 
